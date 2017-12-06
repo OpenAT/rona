538a6a9f8e9b8eaaabf1944516770f259381bdfd
Michael Karrer <michaelkarrer81@gmail.com>
2017-12-06 20:23:15 +0100

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/rona_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'rona_config',
+    'summary': '''FS-Online rona instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance rona
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
